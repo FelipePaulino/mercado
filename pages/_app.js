2c2ec90e43fa0df37a620443e6c3e1552a6f663b
FelipePaulino <viniciusitalodacruz@gmail.com>
2021-07-26 22:28:04 -0300

    finalizado

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import { useEffect, useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from '../src/styles/Global'
+import { Theme } from '../src/styles/themes/theme'
+import Header from '../src/components/Header'
+import Footer from '../src/components/Footer'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [isLogged, setIsLogged] = useState()
+  const tema = Theme
+
+  useEffect(() => {
+    setIsLogged(localStorage.getItem('Logged'))
+  }, []);
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="./logo.png" type="image/x-icon" />
+      </Head>
+      <ThemeProvider theme={tema}>
+        <GlobalStyle />
+        <Header isLogged={isLogged} />
+        <Component {...pageProps} isLogged={isLogged} />
+        <Footer />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
